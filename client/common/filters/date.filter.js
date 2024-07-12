c7913fb06bbad137634165632d9099fc37907c91
shamilfrontend <shoma.alisultanov@yandex.ru>
2024-07-12 18:14:01 +0300

    feat: clear

--- client/common/filters/date.filter.js
+++
@@ -1,5 +0,0 @@
-export default function(value, type) {
-  if (type === 'date') return new Date(value).toLocaleDateString()
-  else if (type === 'time') return new Date(value).toLocaleTimeString()
-  return new Date(value).toLocaleString()
-}
